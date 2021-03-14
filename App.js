989ae22e48940fd45a9bed652d07c74834b58f55
Vincent <booncheng89@gmail.com>
2021-03-14 16:26:05 +0800

    Port over to Expo version

--- App.js
+++ App.js
@@ -1,21 +1,104 @@
-import { StatusBar } from 'expo-status-bar';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+    Image,
+    TouchableOpacity
+} from 'react-native';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// screens
+import { Home } from "./screens/";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import { images, icons, COLORS, FONTS, SIZES } from './constants';
+import { useFonts } from 'expo-font';
+
+const theme = {
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        border: "transparent",
+    },
+};
+
+const Stack = createStackNavigator();
+
+const App = () => {
+
+    const [loaded] = useFonts({
+        "CarmenSans-Regular" : require('./assets/fonts/CarmenSans-Regular.otf'),
+        "CarmenSans-SemiBold" : require('./assets/fonts/CarmenSans-SemiBold.otf'),
+        "CarmenSans-Thin" : require('./assets/fonts/CarmenSans-Thin.otf'),
+        "CocoGothic-Bold" : require('./assets/fonts/CocoGothic-Bold.ttf'),
+        "CocoGothic": require('./assets/fonts/CocoGothic.ttf'),
+    })
+
+    if(!loaded){
+        return null;
+    }
+
+    return (
+        <NavigationContainer theme={theme}>
+            <Stack.Navigator
+                initialRouteName={'Home'}
+            >
+                <Stack.Screen
+                    name="Home"
+                    component={Home}
+                    options={{
+                        title: 'SHOE SELECTOR',
+                        headerStyle: {
+                            //backgroundColor: '#f4511e',
+                        },
+                        headerTintColor: COLORS.lightGray,
+                        headerTitleStyle: {
+                            ...FONTS.navTitle
+                        },
+                        headerLeft: ({ onPress }) => (
+                            <TouchableOpacity
+                                style={{ marginLeft: SIZES.padding }}
+                                onPress={onPress}
+                            >
+                                <Image
+                                    source={icons.menu}
+                                    resizeMode="contain"
+                                    style={{
+                                        width: 25,
+                                        height: 25,
+                                    }}
+                                />
+                            </TouchableOpacity>
+                        ),
+                        headerRight: () => (
+                            <TouchableOpacity
+                                style={{ marginRight: SIZES.padding }}
+                                onPress={() => console.log("Pressed")}
+                            >
+                                <Image
+                                    source={icons.search}
+                                    resizeMode="contain"
+                                    style={{
+                                        width: 30,
+                                        height: 30,
+                                    }}
+                                />
+                            </TouchableOpacity>
+                        ),
+                    }}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+
+export default () => {
+    return <App />;
+};
